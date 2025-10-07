c1b98491167ab3de06dc18d2ec41eb0608fb4907
Juan Carlo <141068249+donjuan029@users.noreply.github.com>
2025-10-07 19:57:05 -0300

    Add files via upload

--- br/com/estoque/servico/ServicoEstoque.java
+++ br/com/estoque/servico/ServicoEstoque.java
@@ -1,1 +1,19 @@
+package br.com.estoque.servico;
+
+import br.com.estoque.modelo.Produto;
+
+/**
+ * Classe de Serviço (Lógica): Contém regras de negócio.
+ */
+public class ServicoEstoque {
+
+    public void adicionarProduto(Produto p, int valor) {
+        if (valor > 0) {
+            p.adicionarEstoque(valor);
+            System.out.println("LOG: Adicionado " + valor + " unidades de " + p.getNome() + ".");
+        } else {
+            System.out.println("LOG: Valor de adição deve ser positivo.");
+        }
+    }
+}
 
